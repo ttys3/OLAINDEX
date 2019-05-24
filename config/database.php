ef0a0a2a36db6a6d8e79baf6337f07d19b433309
荒野無燈 <i@80x86.io>
2019-05-25 01:22:51 +0800

    fixup config/database.php for Predis

--- config/database.php
+++ config/database.php
@@ -110,16 +110,20 @@ return [
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'parameters'=>[
+                'password'=>env('REDIS_PASSWORD', null),
+                'database' => env('REDIS_DB', 0),
+            ],
         ],
 
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
+            'parameters'=>[
+                'password'=>env('REDIS_PASSWORD', null),
+                'database' => env('REDIS_CACHE_DB', 1),
+            ],
         ],
 
     ],
